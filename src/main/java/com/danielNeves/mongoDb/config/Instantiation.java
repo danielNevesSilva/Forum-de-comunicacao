ac13d0fe10caf1edf02ecb45e05a52618d7248ec
Daniel Neves <neves2590@gmacil.com>
2023-07-02 12:44:44 -0300

    peojecao dosdadis di author com DTO

--- src/main/java/com/danielNeves/mongoDb/config/Instantiation.java
+++ src/main/java/com/danielNeves/mongoDb/config/Instantiation.java
@@ -1,5 +1,6 @@
 package com.danielNeves.mongoDb.config;
 
+import com.danielNeves.mongoDb.DTO.AuthorDTO;
 import com.danielNeves.mongoDb.domain.Post;
 import com.danielNeves.mongoDb.domain.User;
 import com.danielNeves.mongoDb.repository.PostRepository;
@@ -33,12 +34,11 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-        Post post1 = new Post(null,sdf.parse("21/03/2023"),"Partiu viagem", "Vou viajar para são paulo. Abraços", maria);
-        Post post2 = new Post(null,sdf.parse("23/03/2023"),"Bom dia", "Acordei feliz hoje!", maria);
-
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null,sdf.parse("21/03/2023"),"Partiu viagem", "Vou viajar para são paulo. Abraços", new AuthorDTO(maria));
+        Post post2 = new Post(null,sdf.parse("23/03/2023"),"Bom dia", "Acordei feliz hoje!",new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
