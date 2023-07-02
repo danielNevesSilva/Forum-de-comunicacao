25b9c430b808cb59f0602dcade86569f50d37f8e
Daniel Neves <neves2590@gmacil.com>
2023-07-02 14:53:24 -0300

    consulta com varios cliterios

--- src/main/java/com/danielNeves/mongoDb/resource/PostResource.java
+++ src/main/java/com/danielNeves/mongoDb/resource/PostResource.java
@@ -13,6 +13,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,10 +29,22 @@ public class PostResource {
         return ResponseEntity.ok().body(obj);
     }
 
-    @RequestMapping(value="titlesearch", method = RequestMethod.GET)
-    public ResponseEntity<List<Post>> findBytitle(@RequestParam(value = "text", defaultValue = "")String text) throws UnsupportedEncodingException {
+    @RequestMapping(value="/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findBytitle(@RequestParam(value = "text", defaultValue = "")String text) {
         text = URL.decodeParam(text);
         List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @RequestMapping(value="/fullsearch", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value="text", defaultValue="") String text,
+            @RequestParam(value="minDate", defaultValue="") String minDate,
+            @RequestParam(value="maxDate", defaultValue="") String maxDate)  {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = service.fullSearch(text, min, max);
         return ResponseEntity.ok().body(list);
     }
 }
