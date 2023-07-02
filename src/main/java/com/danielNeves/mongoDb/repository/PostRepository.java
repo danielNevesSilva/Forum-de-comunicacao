be8c445c2e7f598f221af6be01a50a79812fd8b7
Daniel Neves <neves2590@gmacil.com>
2023-07-02 13:41:57 -0300

    Consulta simples com @Query

--- src/main/java/com/danielNeves/mongoDb/repository/PostRepository.java
+++ src/main/java/com/danielNeves/mongoDb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.danielNeves.mongoDb.repository;
 import com.danielNeves.mongoDb.domain.Post;
 import com.danielNeves.mongoDb.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,8 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String > {
 
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
+
 }
