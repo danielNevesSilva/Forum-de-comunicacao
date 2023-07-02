f6badf47ba4899068435788eb0feb07764ec2251
Daniel Neves <neves2590@gmacil.com>
2023-07-02 12:53:43 -0300

    Referenciando os posts do user

--- src/main/java/com/danielNeves/mongoDb/domain/User.java
+++ src/main/java/com/danielNeves/mongoDb/domain/User.java
@@ -1,11 +1,14 @@
 package com.danielNeves.mongoDb.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 @Document(collection = "user")
 public class User implements Serializable {
@@ -15,6 +18,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     public User(){
     }
@@ -42,6 +48,11 @@ public class User implements Serializable {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
